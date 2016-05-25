a4c2ccdcbffd99aab4b2e5c4c509456603b4d190
Roger Stein <rogerstein@gmail.com>
2016-05-25 11:23:36 -0300

    Atendimento prioritario

--- queue3.R
+++ queue3.R
@@ -1,5 +1,5 @@
 #simulador para fila única e n servos
-setwd("C:\\Users\\i826950\\Desktop\\Personal\\Unisinos\\model&simul")
+options(stringsAsFactors = FALSE)
 
 #state variables
 simClock <- 0
@@ -8,7 +8,8 @@ queue <-
   data.frame(
     clientNum <- numeric(),
     arrivalTime <- numeric(),
-    chamada <- character()
+    chamada <- character(),
+    stringsAsFactors = F
   )
 
 #statistics
@@ -18,7 +19,7 @@ delay <- 0
 areaQ <- 0
 areaB <- 0
 
-data <- read.csv(file="data18.csv", stringsAsFactors = F)
+data <- read.csv(file="data1718.csv", stringsAsFactors = F)
 data$arrivalTimestamp <- as.POSIXct(data$arrivalTimestamp)
 data$servStartTimestamp <- as.POSIXct(data$servStartTimestamp)
 
@@ -27,7 +28,7 @@ timeNextArrival <- data[clientNum, "arrivalTimestamp"]
 timeNextDeparture <- Inf # just to make shure the first event is an arrival
 
 #stop criteria (number of served customers)
-reqCustServed <- 200 
+reqCustServed <- 500 
 
 update <- function(curTime, event){
   timeLast <<- simClock
@@ -52,13 +53,14 @@ log <- function(event, clientNum, chamada){
                              ))
 }
 
-numServers <- 5
+numServers <- 9
 servers <- 
   data.frame(
-    busy=rep(F, numServers),
+    busy=rep(FALSE, numServers),
     depTime=Inf,
     clientNum=0,
-    chamada=""
+    chamada="",
+    stringsAsFactors = F
   )
 
 allServersBusy <- function(){
@@ -71,11 +73,14 @@ allocateServer <- function(clientNum, chamada) {
       which(!servers$busy) 
     else 
       sample(which(!servers$busy), 1)
-  servers[choosenServer, ] <<- list(T, getDepartTime(clientNum), clientNum, chamada)
+  servers[choosenServer, ] <<- list(busy=T, 
+                                    depTime=getDepartTime(clientNum), 
+                                    clientNum=clientNum, 
+                                    chamada=chamada)
 }
 
 deallocateServer <- function(server) {
-  servers[server, ] <<- c(F, Inf, 0, "")
+  servers[server, ] <<- list(FALSE, Inf, 0, "")
 }
 
 getDepartTime <- function(clientNum) {
@@ -91,10 +96,16 @@ while(numCustServed < reqCustServed){
     if(allServersBusy()){
       if(substr(chamada, 2, 2)=="P"){
         #customer goes to start of the queue
-        queue <- rbind(data.frame(clientNum=arrivingClient, arrivalTime=simClock, chamada=chamada), queue)      
+        queue <- rbind(data.frame(clientNum=arrivingClient, 
+                                  arrivalTime=simClock, 
+                                  chamada=chamada), 
+                       queue)      
       } else {
         #customer goes to end of the queue
-        queue <- rbind(queue, data.frame(clientNum=arrivingClient, arrivalTime=simClock, chamada=chamada))
+        queue <- rbind(queue, 
+                       data.frame(clientNum=arrivingClient, 
+                                  arrivalTime=simClock, 
+                                  chamada=chamada))
       }
     } else {
       allocateServer(clientNum, chamada)
@@ -103,63 +114,23 @@ while(numCustServed < reqCustServed){
     }
     log("arrive", arrivingClient, chamada=chamada)
     clientNum <- clientNum + 1
-    timeNextArrival <- data[clientNum, "arrivalTimestamp"]
+    timeNextArrival <- if (is.na(data[clientNum, "arrivalTimestamp"])) Inf else data[clientNum, "arrivalTimestamp"]
   }else{
     update(timeNextDeparture, "departure")
     departingClientNum <- servers[serverNextDeparture, "clientNum"]
     departingClientChamada <- servers[serverNextDeparture, "chamada"]
     deallocateServer(serverNextDeparture)
     numCustServed <- numCustServed + 1
-    timeNextDeparture <- min(servers$depTime)
-    serverNextDeparture <- which.min(servers$depTime)
     if(nrow(queue)>0){
       delay <- simClock - as.numeric(queue[1, "arrivalTime"])
       delaysTotal <- delaysTotal + delay
       allocateServer(clientNum=queue[1, "clientNum"], chamada=queue[1, "chamada"])
-      timeNextDeparture <- min(servers$depTime)
-      serverNextDeparture <- which.min(servers$depTime)
       queue <- queue[-1,]
     }
-    log("depart", departingClientNum, chamada=departingClientChamada)
+    timeNextDeparture <- min(servers$depTime)
+    serverNextDeparture <- which.min(servers$depTime)
+    log(event="depart", clientNum=departingClientNum, chamada=departingClientChamada)
   }
 }
 
-op <- par(mfrow = c(2,1),
-          oma = c(1,1,1,1) + 0.1,
-          mar = c(4,1,2,1) + 0.1)
-
-plot(stepfun(logDF$time, c(0, logDF$queue)), 
-     main="Queue size over time", 
-     xlab="", ylab="")
-abline(h=areaQ/simClock, col="red")
-
-mtext(paste0("[red] Estimated average queue size (q): ", 
-             sprintf("%1.2f", areaQ/simClock)),
-      side=1, line=2)
-mtext(paste0("Estimated averge delay (d): ",
-             sprintf("%1.2f", delaysTotal/numCustServed), " u.t."), 
-      side=1, line=3) 
-
-plot(stepfun(logDF$time, c(0, logDF$state)), 
-     main="Server state over time",
-     xlab="", ylab="")
-
-mtext(paste0("Estimated utilization (b): ",
-             sprintf("%0.2f%%", 100*areaB/simClock)),
-      side=1, line=2)
-
-print(logDF)
-
-
-#N1 = 16/maio -> avaliação intermediaria !!!
-#N2 = 6/jun -> avaliação final
-#PE = 13/Jun -> Prova Extra
-#NF1 = (0.5*N1)+(0.5*N2)
-#Recuperação
-#NF2 = (0.55*NF1)+(0.45*PE) 
-
-
-
-t.test
-
-
+write.csv()
